046dc16163749ad67b0297ee696da03a41f8a0f0
Sergii <wheelyhog@gmail.com>
2023-05-08 18:55:50 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+// let swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     effect: "fade",
+//     loop: true,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     autoplay: {
+//         delay: 6000,
+//         disableOnInteraction: false,
+//     },
+// });
+
 let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    effect: "fade",
+    slidesPerView: 3,
+    spaceBetween: 10,
     loop: true,
     pagination: {
         el: ".swiper-pagination",
